cb9ef7425cfd1d204c400ed8dbc9fe9a6396f20b
sergey radionov <rsatom@gmail.com>
2019-12-09 21:34:17 +0700

    Extracted from native WebRTSP client/server repo

---
+++ RtspRequest.mjs
@@ -0,0 +1,14 @@
+export default class Request
+{
+    constructor() {
+        this.method = null;
+        this.uri = null;
+        this.protocol = null;
+        this.cseq = null;
+        this.session = null;
+
+        this.headerFields = new Map();
+
+        this.body = null;
+    }
+}
---
+++ RtspResponse.mjs
@@ -0,0 +1,14 @@
+export default class Response
+{
+    constructor() {
+        this.protocol = null;
+        this.statusCode = null;
+        this.reasonPhrase = null;
+        this.cseq = null;
+        this.session = null;
+
+        this.headerFields = new Map();
+
+        this.body = null;
+    }
+}
